507457a9193df2fd66288f1ed05573343b56aeb3
Olesia <89938016+mrsdoa@users.noreply.github.com>
2023-02-01 09:19:18 +0300

    Update main.py

--- module/main.py
+++ module/main.py
@@ -1,13 +1,15 @@
 from application.salary import calculate_salary
 from application.db.people import get_employees
 from datetime import date
+import emoji
 
 
 if __name__ == '__main__':
     calculate_salary(10) # если импортируем полностью модуль то название модуля.объект
     get_employees('Oleg')
     print(date.today())
-
+    emoji.print_simbol(question=input('Тебе нравится Python? Введи: "Да", "Нет":'))
+    
     # найти интересный пакет в requirements указать его версию,
     # при желании написать программу с этим пакетом
 
